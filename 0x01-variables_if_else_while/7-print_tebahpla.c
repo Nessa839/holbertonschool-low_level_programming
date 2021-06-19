2edb7ebd53af0cc2f65a1de2970d1532b0eabb33
Nessa839 <vanessatalero1@gmail.com>
2021-06-18 19:34:55 -0700

    Added a script that prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ main - shows the alphabet in reverse. 
+ * Return: goes back to zero.
+*/
+int main(void)
+{
+int alphabet = 122;
+while (alphabet >= 97)
+{
+putchar(alphabet);
+alphabet--;
+}
+putchar(10);
+return(0);
+}
