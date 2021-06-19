b6cad8844dbca092e5d29d5fadc7d62dd8f8e230
Nessa839 <vanessatalero1@gmail.com>
2021-06-18 19:12:27 -0700

    Added a program that pritns single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main - prints a number of base 10
+ * Return: Goes back to zero
+ */
+
+int main(void)
+{
+int number;
+
+number = 48;
+while  (number < 58)
+{
+putchar(number);
+number++;
+}
+putchar(10);
+
+return (0);
+}
