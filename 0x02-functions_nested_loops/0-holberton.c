631c96ba015073fca834d3468b3485d160aac974
Nessa839 <vanessatalero1@gmail.com>
2021-06-22 16:06:18 -0700

    added missing files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+* main - prints holberton
+* Return: Goes back to zero 
+*/
+int main (void)
+{
+
+  putchar(72);
+  putchar(111);
+  putchar(108);
+  putchar(98);
+  putchar(101);
+  putchar(114);
+  putchar(116);
+  putchar(111);
+  putchar(110);
+  putchar(10);
+
+  return (0);
+}
