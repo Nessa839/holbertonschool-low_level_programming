a1fc8ad65e41309a74c3b4a3b496556f6babe7e7
Nessa839 <vanessatalero1@gmail.com>
2021-06-22 16:11:12 -0700

    Fixed the file following betty's instructions

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,21 +2,20 @@
 
 /**
 * main - prints holberton
-* Return: Goes back to zero 
+* Return: Goes back to zero
 */
-int main (void)
+int main(void)
 {
 
-  putchar(72);
-  putchar(111);
-  putchar(108);
-  putchar(98);
-  putchar(101);
-  putchar(114);
-  putchar(116);
-  putchar(111);
-  putchar(110);
-  putchar(10);
-
-  return (0);
+putchar(72);
+putchar(111);
+putchar(108);
+putchar(98);
+putchar(101);
+putchar(114);
+putchar(116);
+putchar(111);
+putchar(110);
+putchar(10);
+return (0);
 }
