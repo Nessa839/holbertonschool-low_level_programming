d649f4635c8890606067259d7533e84b58e7749e
Nessa839 <vanessatalero1@gmail.com>
2021-06-23 21:42:17 -0700

    Added a function that shows the value of an integrer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _abs - shows the value of an integrer
+* @n: number
+* Return: returns the result
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+
+else
+{
+return (-n);
+}
+}
