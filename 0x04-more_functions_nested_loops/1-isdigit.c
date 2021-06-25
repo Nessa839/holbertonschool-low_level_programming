193b3a02297190affb25d2af0589f4abc09a0a9d
Nessa839 <vanessatalero1@gmail.com>
2021-06-25 12:39:33 -0700

    Added a function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _isdigit - checks for the digit
+* @c: character
+* Return: either 0 or 1
+*/
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+	return (1);
+else
+	return (0);
+}
