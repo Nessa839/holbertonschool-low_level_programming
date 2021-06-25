71c35582c8cf2f5cb7e77bf2fa308adebb4aa996
Nessa839 <vanessatalero1@gmail.com>
2021-06-24 21:31:13 -0700

    first attempt point 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - shows if integer is positive or negative
+ * Return: goes back to zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
