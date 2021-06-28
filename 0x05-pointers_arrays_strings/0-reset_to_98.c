cb6bcac6168975c130cb033c1caed34a87be88a8
Nessa839 <vanessatalero1@gmail.com>
2021-06-28 13:06:27 -0700

    added a function that takes the pointer to an int and makes the value 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - resets the value of the pointer to 98
+* @n: makes the pointer into an int
+*
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory I will start to use pointers and arrays.
