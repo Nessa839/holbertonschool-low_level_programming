3e4e5f23e60cb2c8f078045b303ac8f4517448c1
Nessa839 <vanessatalero1@gmail.com>
2021-07-01 12:51:28 -0700

    Added point 5, fixed 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints quote
+ * Return: goes back to zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
