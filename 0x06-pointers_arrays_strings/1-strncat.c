ae2ca32501bb731f8233f7d2b49c5ebb81cc8c29
Nessa839 <vanessatalero@gmail.com>
2021-07-02 19:10:53 -0500

    added library and a function that concatenates bytes from a str to another

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* _strncat - concatenates bytes from a file to another
+* @src: string
+* @dest: destination
+* @n: byes to concatenate
+* Return: results
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
