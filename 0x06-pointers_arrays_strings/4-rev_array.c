c93e078bdf33e19a24cbc1d5072690020fcd9646
Nessa839 <vanessatalero@gmail.com>
2021-07-02 21:02:23 -0500

    Added a function that reverses the content of an array or integrers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the array
+ * @a: array to be reversed
+ * @n:
+ * Return: void
+*/
+
+
+
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, j;
+	n = n -1;
+	while (i < n)
+	{
+		j = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = j;
+		i++;
+		n--;
+
+	}
+}
