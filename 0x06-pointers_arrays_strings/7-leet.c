67cde289f5703282ad0b738b625ad0ffcc14358d
Nessa839 <vanessatalero@gmail.com>
2021-07-02 22:02:18 -0500

    Added a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string
+ * @s: string to encode
+ * Return: enconded string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+	return (s);
+}
