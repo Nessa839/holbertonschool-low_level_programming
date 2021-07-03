dbb65cbc5fe3d52ba8e78224d37b8ffa51bbc7e0
Nessa839 <vanessatalero@gmail.com>
2021-07-02 21:43:10 -0500

    Added correct file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes the words of a string
+ * @str: string to capitalize
+ * Return: changed string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char points[13] = {
+	',', ';', '.', '!', '?',
+	'(', ')', '{', '}', ' ',
+	'\t', '\n', '"'
+	};
+
+	if (str == 0)
+		return (0);
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; points[j] != '\0'; j++)
+		{
+			if (str[i] == points[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+
+	return (str);
+}
